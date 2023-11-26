9f06fe6a038f8dbdc2ed760c48d39e8267d7d035
ranjanDeepak <ranjandeepak071@gmail.com>
2023-11-27 03:13:24 +0530

    Making E-Commerce Site By using React Js

---
+++ src/Components/Description/DescriptionBox.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">
+            Description
+        </div>
+        <div className="descriptionbox-nav-box fade">
+            Reviews(121)
+        </div>
+      </div>
+      <div className="descriptionbox-description">
+        <p> An e-comerce website is an online platform that facilitate
+            buyinhg and selling of products or service over the internet
+            derves as a virtual marketplace where business and individual showcases their products
+            trnsaction without the need for a physical presenee
+        </p>
+        <p>
+        An e-comerce website is an online platform that facilitate
+            buyinhg and selling of products or service over the internet
+            derves as a virtual marketplace where business and individual showcases their products
+            trnsaction without the need for a physical presenee 
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
